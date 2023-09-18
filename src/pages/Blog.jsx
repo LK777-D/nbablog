b3e8200c5297c797543898841d751de25aba698c
LK777-D <luka.dolbaia17@gmail.com>
2023-09-18 17:22:27 +0400

    screen

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -14,7 +14,7 @@ const Blog = () => {
       <h1 className="text-white font2 text-sm lg:text-3xl p-1 text-center underline my-7">
         Stay informed and entertained with our NBA blog
       </h1>
-      <section className="p-2 flex flex-col md:flex md:flex-row gap-3 md:p-10 ">
+      <section className="p-2 flex flex-col md:flex md:flex-row md:justify-center gap-3 md:p-10 ">
         <aside className=" p-5 bg-zinc-900 max-w-[40rem] rounded-lg ">
           <h1 className="text-md text-zinc-400 underline mb-5 ">
             Around The Legue For The Next Season
